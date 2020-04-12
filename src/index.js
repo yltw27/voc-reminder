f5296dec2d65c4f1c2fd73251bb843468751f3c6
friday27 <yiyinghahalin@gmail.com>
2020-04-12 18:03:13 +0800

    heroku test 3

--- src/index.js
+++ src/index.js
@@ -9,12 +9,38 @@ let bot = linebot({
 });
 
 // 當有人傳送訊息給 Bot 時
-bot.on('message', function (event) {
-  // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
-  event.reply(`你說了 ${event.message.text}`);
+// bot.on('message', function (event) {
+//   console.log(event.message.text);
+//   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
+//   event.reply(`${profile.displayName} 說了 ${event.message.text}`);
+// });
+
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+      let msg = event.message.text;
+      //收到文字訊息時，直接把收到的訊息傳回去，這裏是 echo，就是你問什麼就回答什麼，簡單的對話
+      event.reply(msg).then(function(data) {
+          // 傳送訊息成功時，可在此寫程式碼
+          console.log(msg);
+      }).catch(function(error) {
+          // 傳送訊息失敗時，可在此寫程式碼
+          console.log('錯誤產生，錯誤碼：'+error);
+      });
+  }
 });
 
-// Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
-bot.listen('/', process.env.PORT || 5000, function () {
-  console.log('全國首家LINE線上機器人上線啦！！');
+const app = express();
+
+// bot.parser() 是 LINE Bot 的傳過來的資料，以及 JSON 解析
+const linebotParser = bot.parser();
+app.post('/webhook', linebotParser);
+
+const server = app.listen(process.env.PORT || 3000, function() {
+  let port = server.address().port;
+  console.log('目前的port是', port);
 });
+
+// // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
+// bot.listen('/', process.env.PORT || 5000, function () {
+//   console.log('全國首家LINE線上機器人上線啦！！');
+// });
